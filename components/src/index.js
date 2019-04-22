740795e09e0b49b0bc39c3fb8753a59baa22d1d5
xinbo wang <xinbo.wang.dt@gmail.com>
2019-04-22 13:33:02 -0600

    Add component project

---
+++ components/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetail'
+import faker from "faker"
+import ApprovalCard from './approvalCard'
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <h4>Warining!</h4>
+                <div>Are you sure?</div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="4:30PM"
+                    context="Nice blog"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="4:50PM"
+                    context="not bed"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+);
