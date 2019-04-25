10b97e8ea0d192c69f6d79df301fd2752c46c6b4
xinbo wang <xinbo.wang.dt@gmail.com>
2019-04-25 16:21:54 -0600

    finish Season Display function

--- seasons/src/SeasonDisply.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const SeasonDisply = () => {
-    return <div>Display!</div>
-};
-
-export default SeasonDisply
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,24 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null };
-
+    //position to show the data
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => console.log(err)
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({errorMessage: err.message })
         );
+    }
+    //Place to do more data loading when state/props change
+    componentDidUpdate(prevProps, prevState, snapshot) {
+    }
+    //Place to do cleanup
+    componentWillUnmount() {
+    }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        return <Spinner message=' Please accept location request ' />
     }
 
     render() {
-
-        return <div>Latitude: {this.state.lat}</div>
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
