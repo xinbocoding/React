f606789a648d5e136928113e6118a58df0e9203d
Xinbo Wang <xinbo.wang.dt@gmail.com>
2019-05-14 15:39:20 -0600

    Update youtube.js

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAeiUZVQeBk7Wkdds-FwVeEn0xMFTVNhtU';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
