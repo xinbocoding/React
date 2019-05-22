1ae5d0802a99634472c7aaf3cf8eabb85ce7bbaf
xinbo wang <xinbo.wang.dt@gmail.com>
2019-05-22 17:54:55 -0600

    create song App

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAeiUZVQeBk7Wkdds-FwVeEn0xMFTVNhtU';
+const env = process.env.REACT_APP_KEY;
+
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         maxResults: 5,
-        key: KEY
+        key: env
     }
 });
