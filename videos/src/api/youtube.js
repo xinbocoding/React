ae9412e1c3f05501c041acf64e3cb21b63a012e8
xinbo wang <xinbo.wang.dt@gmail.com>
2019-05-22 17:59:37 -0600

    update videos App

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
 const env = process.env.REACT_APP_KEY;
 
-=======
-const KEY = '';
->>>>>>> f606789a648d5e136928113e6118a58df0e9203d
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
