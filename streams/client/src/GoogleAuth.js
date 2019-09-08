1a7f6b8b3994312d925ec2edc8b6c9f17c2c9eef
xinbo wang <xinbo.wang.dt@gmail.com>
2019-09-08 13:46:11 -0700

    update lodash version

--- streams/client/src/GoogleAuth.js
+++ streams/client/src/GoogleAuth.js
@@ -3,21 +3,37 @@ import React from 'react'
 const clientId = process.env.REACT_APP_AUTH_ID;
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: clientId,
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange() {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't have auth</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I signed in</div>
+    }
+  }
   render() {
     return (
       <div>
-        GoogleAuth
-    </div>
+        {this.renderAuthButton()}
+      </div>
     )
   }
 }
